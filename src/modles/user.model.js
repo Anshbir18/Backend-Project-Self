1cc2541ab7e074d46a5169a9c37aa57677b10094
Anshbir-Singh <anshbir181@gmail.com>
2024-02-07 18:25:11 +0530

    User and video model with hooks and JWT 7:03 done

--- src/modles/user.model.js
+++ src/modles/user.model.js
@@ -1,5 +1,7 @@
  import mongoose  from "mongoose";
 
+ import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
  const userSchema= new mongoose.Schema({
     username:{
         type:String,
@@ -51,5 +53,48 @@
  
  )
 
+
+ userSchema.pre("save", function(next){
+    if(!this.isModified(this.password)){
+        return next(); //do nothing if the password is not changed
+    }
+
+    this.password = bcrypt.hash(this.password,10)
+    next();
+ })
+
+
+//  checking if the password is correct or not
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
  export const User=mongoose.model('User',userSchema)  //stored in db as users
